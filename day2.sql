ea9a09895b1d9ccd200b6add01df8aece186f203
Cihad Atınç <atinccihad@gmail.com>
2022-03-12 00:34:48 +0300

    Q

---
+++ day2.sql
@@ -0,0 +1,14 @@
+
+/*============================================================
+          TABLO OLUSTURMA(CRUD-- create)
+=============================================================*/
+use sys;
+create table student
+(id VARCHAR(4),
+name VARCHAR(25),
+age int
+);
+INSERT INTO student VALUES( "1001","MEHMET ALİ", 25);
+INSERT INTO student VALUES("1002", "AYSE YILMAZ", 34);
+INSERT INTO student VALUES("1003", "JOHN STAR", 56);
+INSERT INTO student VALUES("1004", "MARY BROWN", 17);
